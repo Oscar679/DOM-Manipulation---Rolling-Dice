84d6d56433f3b1425d9975dffde51ecfaa0daf23
Oscar Ekberg <oscarekberg@hotmail.com>
2025-01-17 12:40:34 +0100

    fixed error with jsdoc interpreting IIFE

--- src/js/Document.js
+++ src/js/Document.js
@@ -1,52 +1,55 @@
+
+/** 
+ * The initializing Document function.
+ * Initializes event listeners for various icons.
+ * 
+ * @class
+ */
+function Document() {
+    this.init();
+}
+
+/**
+ * An IIFE that initializes the Document class.
+ */
 (function () {
-    /** 
-     * The initializing Document function.
-     * Initializes event listeners for various icons.
-     * 
-     * @class
-     */
-    function Document() {
-        this.init();
-    }
-
-    /**
-     * Various icons are extracted from the DOM.
-     * 
-     * Event listeners are added to the DOM-elements.
-     * 
-     * The event listeners trigger their individual application creator-method.
-     * @returns {void}
-     */
-    Document.prototype.init = function () {
-        var diceBtn = document.getElementById("icon-dice");
-        diceBtn.addEventListener("click", this.createDiceApp.bind(this));
-
-        var clockBtn = document.getElementById("icon-clock");
-        clockBtn.addEventListener("click", this.createClockApp.bind(this));
-    }
-
-    /**
-     * Instantiates the dice application by creating a new instance of CreateDiceApp.
-     * 
-     * @returns {void}
-     */
-    Document.prototype.createDiceApp = function () {
-        new CreateDiceApp();
-    }
-
-    /**
-     * Instantiates the clock application by creating a new instance of CreateClockApp.
-     * 
-     * @returns {void}
-     */
-    Document.prototype.createClockApp = function () {
-        new CreateClockApp();
-    }
-
     function initDocumentOnLoad() {
         new Document();
     }
 
-    // Attach the event listener
     document.addEventListener("DOMContentLoaded", initDocumentOnLoad);
 })();
+
+/**
+ * Various icons are extracted from the DOM.
+ * 
+ * Event listeners are added to the DOM-elements.
+ * 
+ * The event listeners trigger their individual application creator-method.
+ * @returns {void}
+ */
+Document.prototype.init = function () {
+    var diceBtn = document.getElementById("icon-dice");
+    diceBtn.addEventListener("click", this.createDiceApp.bind(this));
+
+    var clockBtn = document.getElementById("icon-clock");
+    clockBtn.addEventListener("click", this.createClockApp.bind(this));
+}
+
+/**
+ * Instantiates the dice application by creating a new instance of CreateDiceApp.
+ * 
+ * @returns {void}
+ */
+Document.prototype.createDiceApp = function () {
+    new CreateDiceApp();
+}
+
+/**
+ * Instantiates the clock application by creating a new instance of CreateClockApp.
+ * 
+ * @returns {void}
+ */
+Document.prototype.createClockApp = function () {
+    new CreateClockApp();
+}
